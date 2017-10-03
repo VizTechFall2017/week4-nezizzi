6faf406c7a7d89330ce75224c2c5d30056b27523
nezizzi <zizzi.n@husky.neu.edu>
2017-10-03 18:48:15 -0400

    invclass fixes!

--- makeButton/index.js
+++ makeButton/index.js
@@ -2,10 +2,19 @@ var svg = d3.select('svg');
 
 /* Your code goes here */
 
+d3.csv('./data.csv', function(dataIn){
+    console.log(dataIn);
 
-function buttonClicked(){
-    //do something
-    console.log('here');
-}
+    svg.selectAll('circle')
+        .data(dataIn)
+        .enter()
+        .append('circle')
+        .attr(cx, '100')
+        .attr(cy, '100')
+        .attr()
+
+});
+
+
 
 
