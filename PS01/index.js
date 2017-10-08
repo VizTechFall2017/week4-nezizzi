09f75bba54ff1548c788db50524f5e75a67859a9
nezizzi <zizzi.n@husky.neu.edu>
2017-10-07 21:47:13 -0400

    axes moved

--- PS01/index.js
+++ PS01/index.js
@@ -6,14 +6,30 @@ var allData;
 var male;
 var female;
 
+//axes
 var ScaleX = d3.scaleLinear().domain([0,30]).range([0, 600]);
 var ScaleY = d3.scaleLinear().domain([0,30]).range([400, 0]);
+svg.append('g')
+    .attr('transform', 'translate(0,400)')
+    .call(d3.axisBottom(ScaleX));
+svg.append('g')
+    .call(d3.axisLeft(ScaleY));
+svg.append('text')
+    .text('DANCERS')
+    .attr('transform','translate(300, -20)')
+    .style('text-anchor','middle');
+svg.append('text')
+    .text('Age Dancer Began Training')
+    .attr('transform','translate(250, 440)');
+svg.append('text')
+    .text('Age Professional Career Began')
+    .attr('transform', 'translate(-50,300)rotate(270)');
 
+
+//bring in the data!
 d3.csv('./data.csv', function(dataIn){
 
     allData = dataIn;
-    //console.log(allData);
-
     //save the objects from the .csv with female
     female = dataIn.filter(function(d){
         return d.A2GENDER == 1;
@@ -30,13 +46,6 @@ d3.csv('./data.csv', function(dataIn){
         .enter()
         .append('circle')
         .attr('class','dataPoints');
-
-    //axes
-    svg.append('g')
-        .attr('transform', 'translate(0,400)')
-        .call(d3.axisBottom(ScaleX));
-    svg.append('g')
-        .call(d3.axisLeft(ScaleY));
 
 
     //call the drawPoints function below, and hand it the data2016 variable with the 2016 object array in it
